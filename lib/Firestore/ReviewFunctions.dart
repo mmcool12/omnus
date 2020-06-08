543eea46b90b962a0d5f2e9c3a86f58384074516
Maurice Landers III <35241436+mmcool12@users.noreply.github.com>
2020-06-07 19:25:35 -0500

    reviews now stored in chef doc

--- lib/Firestore/ReviewFunctions.dart
+++ lib/Firestore/ReviewFunctions.dart
@@ -8,14 +8,26 @@ class ReviewFunctions {
     return await _db.collection('reviews').where('chefId', isEqualTo: chefId).getDocuments();
   }
 
-  Future<DocumentReference> createReview(Order order, String description, double rating, String title) async {
-    return await _db.collection('reviews').add({
-      'buyerId' : order.buyerId,
-      'buyerName' : order.buyerName,
-      'chefId' : order.chefId,
-      'description' : description,
-      'rating' : rating,
-      'title' : title
+  Future<void> createReview(Order order, String description, double rating, String title) async {
+    // return await _db.collection('reviews').add({
+    //   'buyerId' : order.buyerId,
+    //   'buyerName' : order.buyerName,
+    //   'chefId' : order.chefId,
+    //   'description' : description,
+    //   'rating' : rating,
+    //   'title' : title
+    // });
+    return await _db.collection('chefs').document(order.chefId).updateData({
+      'reviews' : FieldValue.arrayUnion([
+        {
+          'buyerId' : order.buyerId,
+          'buyerName' : order.buyerName,
+          'chefId' : order.chefId,
+          'description' : description,
+          'rating' : rating,
+          'title' : title
+        }
+      ])
     });
   }
 
