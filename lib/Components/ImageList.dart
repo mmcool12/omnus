f3d39a2e44594784725808fc328f670ec2cfedbc
Maurice Landers III <35241436+mmcool12@users.noreply.github.com>
2020-06-07 19:24:43 -0500

    update to work with image header

--- lib/Components/ImageList.dart
+++ lib/Components/ImageList.dart
@@ -4,14 +4,12 @@ import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:omnus/Components/ImageSourceModal.dart';
 import 'package:omnus/Firestore/ImageFunctions.dart';
 import 'package:omnus/Models/Chef.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class ImageList extends StatefulWidget {
-  const ImageList({
-    Key key,
-    @required this.height,
-    @required this.chef,
-    this.edit
-  }) : super(key: key);
+  const ImageList(
+      {Key key, @required this.height, @required this.chef, this.edit})
+      : super(key: key);
 
   final double height;
   final Chef chef;
@@ -23,6 +21,7 @@ class ImageList extends StatefulWidget {
 
 class _ImageListState extends State<ImageList> {
   Future<List<dynamic>> getImages;
+  PageController pageController = PageController();
 
   @override
   void initState() {
@@ -32,41 +31,41 @@ class _ImageListState extends State<ImageList> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<dynamic>>(
-        future: getImages,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData &&
-              snapshot.connectionState != ConnectionState.done) {
-            return Container(
-                height: widget.height * .2,
-                child: Center(child: PlatformCircularProgressIndicator()));
-          } else {
-            List<dynamic> images = snapshot.data;
-            if(widget.edit){
-              if (images.length == 0) {
-                images.add('create');
-              } else if (images[images.length - 1] != 'create') {
-                images.add('create');
-              }
-            }
-            return Container(
-              height: widget.height * .2,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: images.length,
-                  itemBuilder: (context, index) {
-                    if (images[index] == 'create') {
-                      return Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: GestureDetector(
+    return Stack(
+      children: <Widget>[
+        FutureBuilder<List<dynamic>>(
+            future: getImages,
+            builder: (context, snapshot) {
+              if (!snapshot.hasData &&
+                  snapshot.connectionState != ConnectionState.done) {
+                return Container(
+                    height: widget.height * .2,
+                    child: Center(child: PlatformCircularProgressIndicator()));
+              } else {
+                List<dynamic> images = snapshot.data;
+                if (widget.edit) {
+                  if (images.length == 0) {
+                    images.add('create');
+                  } else if (images[images.length - 1] != 'create') {
+                    images.add('create');
+                  }
+                }
+                return Container(
+                  child: PageView.builder(
+                      controller: pageController,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: images.length,
+                      itemBuilder: (context, index) {
+                        if (images[index] == 'create') {
+                          return GestureDetector(
                             onTap: () async {
                               await ImageSourceModal()
                                   .showModal(context, 'chef', widget.chef.id);
-                              getImages = ImageFunctions().getChefsImages(widget.chef.images);
+                              getImages = ImageFunctions()
+                                  .getChefsImages(widget.chef.images);
                               this.setState(() {});
                             },
                             child: Container(
-                              width: widget.height * .2,
                               color: Colors.grey[350],
                               child: Center(
                                 child: Column(
@@ -82,31 +81,48 @@ class _ImageListState extends State<ImageList> {
                                 ),
                               ),
                             ),
-                          ));
-                    } else {
-                      // if(images != null) {
-                      //   if(widget.edit && images.length-1 != widget.chef.images.length && images[images.length-1] != 'new'){
-                      //     images.removeAt(images.length-1);
-                      //     images.add('new');
-                      //   }
-                      // }
-                      return Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: Container(
-                            width: widget.height * .2,
+                          );
+                        } else {
+                          // if(images != null) {
+                          //   if(widget.edit && images.length-1 != widget.chef.images.length && images[images.length-1] != 'new'){
+                          //     images.removeAt(images.length-1);
+                          //     images.add('new');
+                          //   }
+                          // }
+                          return Container(
+                            width: MediaQuery.of(context).size.width,
                             color: Colors.transparent,
                             child: CachedNetworkImage(
                               imageUrl: images[index],
                               placeholder: (context, url) => Center(
                                   child: PlatformCircularProgressIndicator()),
-                              errorWidget: (context, url, obj) => Container(color: Colors.blue, child: Text('Hello')),
-                              fit: BoxFit.fitHeight,
+                              errorWidget: (context, url, obj) => Container(
+                                  color: Colors.blue, child: Text('Hello')),
+                              fit: BoxFit.cover,
                             ),
-                          ));
-                    }
-                  }),
-            );
-          }
-        });
+                          );
+                        }
+                      }),
+                );
+              }
+            }),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 16.0),
+          child: Align(
+              alignment: Alignment.bottomCenter,
+              child: SmoothPageIndicator(
+                  controller: pageController, 
+                  count: widget.edit? widget.chef.images.length + 1 : widget.chef.images.length,
+                  effect: SlideEffect(
+                    activeDotColor: Colors.white,
+                    spacing: 8,
+                    dotHeight: 8,
+                    dotWidth: 8
+                  ),
+                )
+              ),
+        )
+      ],
+    );
   }
 }
