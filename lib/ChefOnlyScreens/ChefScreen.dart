f217d84aa724798de8bf8d049e4a25fad811c32a
Maurice Landers III <35241436+mmcool12@users.noreply.github.com>
2020-06-10 22:01:29 -0500

    add functionality to chef profile, active, and  past order buttons

--- lib/ChefOnlyScreens/ChefScreen.dart
+++ lib/ChefOnlyScreens/ChefScreen.dart
@@ -1,9 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:omnus/ChefOnlyScreens/ActiveRequestsScreen.dart';
 import 'package:omnus/ChefOnlyScreens/ChefEditScreenNew.dart';
+import 'package:omnus/ChefOnlyScreens/PastRequestsScreen.dart';
 import 'package:omnus/Firestore/ChefFunctions.dart';
-import 'package:omnus/Firestore/NotificationFunctions.dart';
 import 'package:omnus/Models/User.dart';
 import 'package:provider/provider.dart';
 
@@ -61,76 +62,79 @@ class ChefScreen extends StatelessWidget {
           ),
           body: Column(
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: const Color(0xffffffff),
-                    border:
-                        Border.all(width: 1.0, color: const Color(0x12707070)),
-                    boxShadow: [
-                      BoxShadow(
-                          color: const Color(0x1a000000),
-                          offset: Offset(0, 3),
-                          blurRadius: 6)
-                    ],
+              GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () => Navigator.push(
+                    context, 
+                    platformPageRoute(context: context, builder: (BuildContext context) => ChefEditScreenNew(chefId: user.chefId))
                   ),
-                  child: Center(child: Text('Chef Profile')),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffffffff),
+                      border:
+                          Border.all(width: 1.0, color: const Color(0x12707070)),
+                      boxShadow: [
+                        BoxShadow(
+                            color: const Color(0x1a000000),
+                            offset: Offset(0, 3),
+                            blurRadius: 6)
+                      ],
+                    ),
+                    child: Center(child: Text('Chef Profile')),
+                  ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: const Color(0xffffffff),
-                    border:
-                        Border.all(width: 1.0, color: const Color(0x12707070)),
-                    boxShadow: [
-                      BoxShadow(
-                          color: const Color(0x1a000000),
-                          offset: Offset(0, 3),
-                          blurRadius: 6)
-                    ],
+              GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () => Navigator.push(
+                    context, 
+                    platformPageRoute(context: context, builder: (BuildContext context) => ActiveRequestsScreen(chefId: user.chefId))
                   ),
-                  child: Center(child: Text('New Orders')),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffffffff),
+                      border:
+                          Border.all(width: 1.0, color: const Color(0x12707070)),
+                      boxShadow: [
+                        BoxShadow(
+                            color: const Color(0x1a000000),
+                            offset: Offset(0, 3),
+                            blurRadius: 6)
+                      ],
+                    ),
+                    child: Center(child: Text('Active Requests')),
+                  ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: const Color(0xffffffff),
-                    border:
-                        Border.all(width: 1.0, color: const Color(0x12707070)),
-                    boxShadow: [
-                      BoxShadow(
-                          color: const Color(0x1a000000),
-                          offset: Offset(0, 3),
-                          blurRadius: 6)
-                    ],
+              GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onTap: () => Navigator.push(
+                    context, 
+                    platformPageRoute(context: context, builder: (BuildContext context) => PastRequestsScreen(chefId: user.chefId))
                   ),
-                  child: Center(child: Text('Active Orders')),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: const Color(0xffffffff),
-                    border:
-                        Border.all(width: 1.0, color: const Color(0x12707070)),
-                    boxShadow: [
-                      BoxShadow(
-                          color: const Color(0x1a000000),
-                          offset: Offset(0, 3),
-                          blurRadius: 6)
-                    ],
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffffffff),
+                      border:
+                          Border.all(width: 1.0, color: const Color(0x12707070)),
+                      boxShadow: [
+                        BoxShadow(
+                            color: const Color(0x1a000000),
+                            offset: Offset(0, 3),
+                            blurRadius: 6)
+                      ],
+                    ),
+                    child: Center(child: Text('Past Requests')),
                   ),
-                  child: Center(child: Text('Past Orders')),
                 ),
               ),
             ],
